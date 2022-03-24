37f3de15abb1de1d499dab45dd6e3a63ac3d4656
Hanna Lopatina <an.lopatina050@gmail.com>
2022-03-25 00:28:30 +0200

    Updated components, added create/read

--- WebApiPracticingApp-Angular/src/app/inspection/show-inspection/show-inspection.component.ts
+++ WebApiPracticingApp-Angular/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspectionList$!: Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+  inspectionTypesList: any = [];
+
+  // Map to display data associate with foreign keys
+  inspectionTypesMap: Map<number, string> = new Map();
+
+  // Variables (properties)
+  modalTitle: string = '';
+  activateAddEditInspectionComponent: boolean = false;
+  inspection: any;
+
+  constructor(private service: InspectionApiService) { }
 
   ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypesList$ = this.service.getInspectionTypesList();
+    this.refreshInspectionTypesMap();
   }
 
+  refreshInspectionTypesMap(){
+    this.service.getInspectionTypesList().subscribe(data => {
+      this.inspectionTypesList = data;
+      for (let i = 0; i < data.length; i++) {
+        this.inspectionTypesMap.set(this.inspectionTypesList[i].id, this.inspectionTypesList[i].inspectionName);        
+      }
+    })
+  }
+
+  modalAdd(){
+    this.inspection = {
+      id: 0,
+      status: null,
+      comments: null,
+      inspectionTypeId: null
+    };
+    this.modalTitle = "Add Inspection";
+    this.activateAddEditInspectionComponent = true;
+  }
+
+  modalClose(){
+    this.activateAddEditInspectionComponent = false;
+    this.inspectionList$ = this.service.getInspectionList();
+  }
 }
